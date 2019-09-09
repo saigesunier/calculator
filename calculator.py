54ccff2f47121ea75efb006ddf56401433d126dd
Saige Sunier <ses123@duke.edu>
2019-09-09 12:22:59 -0400

    Wrote function to analyze HDL test results

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 #calculator.py
 
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL < 60:
+        return "Borderline low"
+    else:
+        return "low"
+
 def interface():
     print("My calculator program")
     keep_running = True
